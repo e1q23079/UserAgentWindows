9589e3a3a9acf5f8c7f35f69e301cb2e7825d32d
e1q23079 <e1q23079@oit.ac.jp>
2025-11-11 23:08:11 +0900

    メインスクリプトを追加し、User-Agentを変更するための動的ルールを設定

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("This is a simple JavaScript program.");
+const rules = [
+    {
+        "id": 1,
+        "priority": 1,
+        "action": {
+            "type": "modifyHeaders",
+            "requestHeaders": [
+                {
+                    "header": "User-Agent",
+                    "operation": "set",
+                    "value": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+                }
+            ]
+        },
+        "condition": {
+            "urlFilter": "*",
+            "resourceTypes": [
+                "main_frame",
+                "sub_frame",
+                "xmlhttprequest"
+            ]
+        }
+    }
+];
+
+chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [1],
+    addRules: rules
+});
